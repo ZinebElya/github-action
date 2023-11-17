d3d3e32d744fef879e32fbe5f3cbf3b4db32ff77
ZinebElya <zineb.elyattouti@gmail.com>
2023-11-17 11:56:10 +0100

    Adding a test that fails

--- src/test/java/org/example/GreetingResourceTest.java
+++ src/test/java/org/example/GreetingResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -16,6 +17,11 @@ public class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    public void testFailure() {
+        fail("This test deliberately fails");
     }
 
 }
